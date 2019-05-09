dccdac4f2cb870dea8ba0155a9af74d7968caede
1TAEK <aldald1318@gmail.com>
2019-05-09 22:32:07 +0900

    백업

--- 개발/2016182033 임원택/ScriptTermProject/main/main.py
+++ 개발/2016182033 임원택/ScriptTermProject/main/main.py
@@ -1,12 +1,35 @@
 from tkinter import*
 
-window = Tk()
+class FragmentManager():
+    pass
 
-window.title("날씨어떄")
+class Main():
+    def __init__(self):
+        self.__windowWidth = 400
+        self.__windowHeight = 600
+        self.__backgroundColor = '#%02x%02x%02x' % (224, 255, 255)
 
-background = PhotoImage(file = "")
-backgroundLabel = Label(window, image = background)
-backgroundLabel.pack()
+        # Init Window
+        self.__window = Tk()
+        self.__window.title("날씨어떄")
+        self.__window.geometry("400x600+100+100")
+        self.__window.resizable(False, False)
+        self.__window.configure(bg=self.__backgroundColor)
 
-window.mainloop()
+        # Init Scroll
+        scrollbar = Scrollbar(self.__window)
+        scrollbar.pack(side=RIGHT, fill=Y)
+        text = Text(self.__window, width=40, height=10, wrap=WORD, yscrollcommand=scrollbar.set)
+        text.pack()
+        scrollbar.config(command=text.yview)
+
+    def main(self):
+        self.__window.mainloop()
+
+
+Main().main()
+
+
+
+
 
