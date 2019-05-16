8a068413d9b765b124a5162ebb1fd1c2aa6a85c0
1TAEK <aldald1318@gmail.com>
2019-05-17 01:00:44 +0900

    변수이름변경
    변수이름변경

--- 개발/날씨어때 텀프로젝트/main/main.py
+++ 개발/날씨어때 텀프로젝트/main/main.py
@@ -5,57 +5,57 @@ class FragmentManager():
     pass
 
 class Main():
-    def __init__(self, **kw):
-        self.__windowWidth = 400
-        self.__windowHeight = 600
-        self.__backgroundColor = '#%02x%02x%02x' % (224, 255, 255)
+    def __init__(self):
+        self.__mWindowWidth = 400
+        self.__mWindowHeight = 600
+        self.__mBackgroundColor = '#%02x%02x%02x' % (224, 255, 255)
 
         # Init Window
-        self.__window = Tk()
-        self.__window.title("날씨어떄")
-        self.__window.geometry("400x600+100+100")
-        self.__window.resizable(False, False)
-        self.__window.configure(bg=self.__backgroundColor)
+        self.__mWindow = Tk()
+        self.__mWindow.title("날씨어떄")
+        self.__mWindow.geometry("400x600+100+100")
+        self.__mWindow.resizable(False, False)
+        self.__mWindow.configure(bg=self.__mBackgroundColor)
 
         # Whole Frame
-        self.wholeFrame = Frame(self.__window,bg=self.__backgroundColor)
-        self.wholeFrame.pack()
+        self.mWholeFrame = Frame(self.__mWindow,bg=self.__mBackgroundColor)
+        self.mWholeFrame.pack()
 
         # Init Frame
         # Menu, Main Frame
-        self.menuFrame = Frame(self.wholeFrame,width = self.__windowWidth,height = self.__windowHeight*0.9, bg = self.__backgroundColor);
-        self.menuFrame.pack()
-        self.mainFrame = Frame(self.wholeFrame, bg = self.__backgroundColor);
-        self.mainFrame.pack()
-        self.scrollbar = Scrollbar(self.mainFrame)
-        self.scrollbar.pack(side=RIGHT, fill=Y)
+        self.mMenuFrame = Frame(self.mWholeFrame,width = self.__mWindowWidth,height = self.__mWindowHeight*0.9, bg = self.__mBackgroundColor);
+        self.mMenuFrame.pack()
+        self.mMainFrame = Frame(self.mWholeFrame, bg = self.__mBackgroundColor);
+        self.mMainFrame.pack()
+        self.mScrollbar = Scrollbar(self.mMainFrame)
+        self.mScrollbar.pack(side=RIGHT, fill=Y)
 
         # Fragment
+        frameRate = 0.4
         # Overview Frame 개황프레임
-        self.overviewFrame = Frame(self.mainFrame,width = self.__windowWidth,height = self.__windowHeight*0.4);
-        self.overviewFrame.pack()
-        self.overview = Overview(self.overviewFrame)
-
+        self.mOverviewFrame = Frame(self.mMainFrame,width = self.__mWindowWidth,height = self.__mWindowHeight*frameRate);
+        self.mOverviewFrame.pack()
+        self.mOverview = Overview(self.mOverviewFrame)
 
         # TimeToWeather Frame 시간별 날씨 프레임
-        self.TimeToWeatherFrame = Frame(self.mainFrame,width = self.__windowWidth,height = self.__windowHeight*0.4, bg = "blue")
-        self.TimeToWeatherFrame.pack()
+        self.mTimeToWeatherFrame = Frame(self.mMainFrame,width = self.__mWindowWidth,height = self.__mWindowHeight*frameRate, bg = "blue")
+        self.mTimeToWeatherFrame.pack()
         # DayToWeather Frame 일별 날씨 프레임
-        self.DayToWeatherFrame = Frame(self.mainFrame,width = self.__windowWidth,height = self.__windowHeight*0.4, bg = "green")
-        self.DayToWeatherFrame.pack()
+        self.mDayToWeatherFrame = Frame(self.mMainFrame,width = self.__mWindowWidth,height = self.__mWindowHeight*frameRate, bg = "green")
+        self.mDayToWeatherFrame.pack()
         # AirFresh Frame 공기청정지수 프레임
-        self.AirFreshFrame = Frame(self.mainFrame,width = self.__windowWidth,height = self.__windowHeight*0.4, bg = "yellow")
-        self.AirFreshFrame.pack()
+        self.mAirFreshFrame = Frame(self.mMainFrame,width = self.__mWindowWidth,height = self.__mWindowHeight*frameRate, bg = "yellow")
+        self.mAirFreshFrame.pack()
 
         # Button
         menuButtonwidth = 8
         menuButtonheight = 1
-        Button(self.menuFrame,text = "검색",width = menuButtonwidth, height = menuButtonheight, command = self.search).grid(row=0, column=0,padx = 4, pady=4)
-        Button(self.menuFrame,text = "즐겨찾기",width = menuButtonwidth, height = menuButtonheight, command = self.bookmark).grid(row=0, column=1,padx = 4, pady=4)
-        Button(self.menuFrame,text = "E-Mail",width = menuButtonwidth, height = menuButtonheight, command = self.email).grid(row=0, column=2, padx = 4, pady=4)
-        Button(self.menuFrame,text = "Home",width = menuButtonwidth, height = menuButtonheight, command= self.home).grid(row=0, column=3, padx = 4, pady=4)
-        Button(self.menuFrame,text = "갱신",width = menuButtonwidth, height = menuButtonheight, command = self.refresh).grid(row=0, column=4,padx = 4, pady=4)
-        Button(self.menuFrame,text = "지역",width = menuButtonwidth, height = menuButtonheight, command = self.location).grid(row=1, column=2, pady=10)
+        Button(self.mMenuFrame,text = "검색",width = menuButtonwidth, height = menuButtonheight, command = self.search).grid(row=0, column=0,padx = 4, pady=4)
+        Button(self.mMenuFrame,text = "즐겨찾기",width = menuButtonwidth, height = menuButtonheight, command = self.bookmark).grid(row=0, column=1,padx = 4, pady=4)
+        Button(self.mMenuFrame,text = "E-Mail",width = menuButtonwidth, height = menuButtonheight, command = self.email).grid(row=0, column=2, padx = 4, pady=4)
+        Button(self.mMenuFrame,text = "Home",width = menuButtonwidth, height = menuButtonheight, command= self.home).grid(row=0, column=3, padx = 4, pady=4)
+        Button(self.mMenuFrame,text = "갱신",width = menuButtonwidth, height = menuButtonheight, command = self.refresh).grid(row=0, column=4,padx = 4, pady=4)
+        Button(self.mMenuFrame,text = "지역",width = menuButtonwidth, height = menuButtonheight, command = self.location).grid(row=1, column=2, pady=10)
 
     def search(self):
         pass
@@ -66,12 +66,12 @@ class Main():
     def home(self):
         pass
     def refresh(self):
-        self.overview.update()
+        self.mOverview.update()
     def location(self):
         pass
 
     def main(self):
-        self.__window.mainloop()
+        self.__mWindow.mainloop()
 
 Main().main()
 
