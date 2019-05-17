9134dd903229d353f5ef400d0c1b391d666f6935
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2019-05-17 21:59:10 +0900

    중기기온 xml 파싱완료
    3일후~10일후의 오전, 오후 기온 정보를 가짐
    문제점으로 10, 2, 3, 4, 5 ... 10일 후 정보부터 시작함
    요거 인지하고 있자 - 태준

--- 개발/2014182019 박태준/parseXMLs.py
+++ 개발/2014182019 박태준/parseXMLs.py
@@ -48,8 +48,32 @@ def getMiddleLandWeather():     # 중기육상예보 xml (3일~10일 후 예보 
         xml = resp.read()
         print("XML Document loading complete.")
         dom = parseString(xml)
+        # print(dom.toprettyxml())
+        return dom
+
+
+def getMiddleTemperature():         # 중기기온조회 xml
+    url = 'http://newsky2.kma.go.kr/service/MiddleFrcstInfoService/getMiddleTemperature'
+    queryParams = '?' + 'ServiceKey=8KngOJTE%2Fh%2BjNJwkeXlJsC5d1ShWfQ9YadkSpoLeubDe9cekkO44ShcRAra7hjTk%2BYAzJEui5eYPFVGegxUngw%3D%3D' + \
+                  '&regId=11B10101&tmFc=201905170600'
+
+    request = urllib.request.Request(url + queryParams)
+
+    try:
+        resp = urllib.request.urlopen(request)
+    except urllib.error.URLError as e:
+        print(e.reason)
+        print(parseString(e.read().decode('utf-8')).toprettyxml())
+    except urllib.error.HTTPError as e:
+        print("error code=" + e.code)
+        print(parseString(e.read().decode('utf-8')).toprettyxml())
+    else:
+        xml = resp.read()
+        print("XML Document loading complete.")
+        dom = parseString(xml)
         print(dom.toprettyxml())
         return dom
 
 # TownDocument = getForecastSpaceData()       # 동네예보 xml DOM객체에 저장
-DaysWeatherDoc = getMiddleLandWeather()
+# DaysWeatherDoc = getMiddleLandWeather()     # 중기예보 xml DOM객체에 저장
+DaysTemperatureDoc = getMiddleTemperature()   # 중기기온 xml DOM객체에 저장
