edcc6e96decc7b21df1e1d2aebec08ca13f4a911
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2019-05-22 02:12:33 +0900

    overview모듈 getSpaceFcstData 함수 완성
    동네예보정보에서 하늘, 강수상태, 온도, 최저,최고온도를 추출하여 리스트에 담아 반환하는 함수.
    
    현재 시간에 가장 근접한 예보 시간의 값들을 가져와야해서
    getCloseTime() 함수 구현하였음.
    getCloseTime()은 현재시간에서 가장 근접한 예보시간을 반환함.
    
    추가로 parseXMLs모듈에서 고정 url이 아닌 시간에 따라 다르게 파싱하기 위하여 현재 시간을 가지고 기준 날짜, 시간을 반환하도록  get_baseDateAndTime() 수정완료 하였음.

--- 개발/2014182019 박태준/overview.py
+++ 개발/2014182019 박태준/overview.py
@@ -1,109 +1,52 @@
- # 개황 프레임에서 필요한 xml파일을 파싱하는 모듈
-import urllib.request
-import urllib.parse
-from xml.dom.minidom import parseString
-from xml.etree import ElementTree
-
-loopFlag = 1
-xmlFD = -1
-WeatherDoc = None
-
-def printMenu():
-    print("₩nWelcome! Book Manager Program (xml version)")
-    print("========Menu==========")
-    print("Load xml:  l")
-    print("Print temperature list: b")
-    print("Print dom to xml: p")
-    print("Quit program:   q")
-    print("==================")
-
-def launcherFunction(menu):
-    global WeatherDoc
-    if menu == 'l':
-        WeatherDoc = loadXMLfromAPI()
-    elif menu == 'b':
-        PrintWeatherList(['category','fcstValue'])
-    elif menu == 'p':
-        PrintDOMtoXML()
-    elif menu == 'q':
-        Quit()
-    else:
-        print ("error : unknow menu key")
-
-def loadXMLfromAPI():
-    url = 'http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastSpaceData'
-    queryParams = '?' + 'ServiceKey=8KngOJTE%2Fh%2BjNJwkeXlJsC5d1ShWfQ9YadkSpoLeubDe9cekkO44ShcRAra7hjTk%2BYAzJEui5eYPFVGegxUngw%3D%3D' +\
-        '&base_date=20190517&base_time=0200&nx=60&ny=127&numOfRows=300' \
-        '&pageNo=1'
-
-    request = urllib.request.Request(url + queryParams)
-
-    try:
-        resp = urllib.request.urlopen(request)
-    except urllib.error.URLError as e:
-        print(e.reason)
-        print(parseString(e.read().decode('utf-8')).toprettyxml())
-    except urllib.error.HTTPError as e:
-        print("error code=" + e.code)
-        print(parseString(e.read().decode('utf-8')).toprettyxml())
-    else:
-        xml = resp.read()
-        print("XML Document loading complete.")
-        dom = parseString(xml)
-        return dom
+from parseXMLs import Parser
+import time
 
 
-def PrintDOMtoXML():
-    if checkDocument():
-        print(WeatherDoc.toprettyxml())
-    pass
+def getCloseTime():    # 현재시간에서 가장 근접한 예보 시간을 반환하는 함수.
+    now = time.strftime('%H', time.localtime()) + '00'
+    if '0000' <= now < '0300':
+        closeTime = '0000'
+    elif '0300' <= now < '0600':
+        closeTime = '0300'
+    elif '0600' <= now < '0900':
+        closeTime = '0600'
+    elif '0900' <= now < '1200':
+        closeTime = '0900'
+    elif '1200' <= now < '1500':
+        closeTime = '1200'
+    elif '1500' <= now < '1800':
+        closeTime = '1500'
+    elif '1800' <= now < '2100':
+        closeTime = '1800'
+    elif '2100' <= now < '0000':
+        closeTime = '2100'
+
+    return closeTime
 
 
-def Quit():
-    global loopFlag
-    loopFlag = 0
-    pass
-
-def checkDocument():
-    global WeatherDoc
-    if WeatherDoc == None:
-        print("Error : Document is empty")
-        return False
-    return True
-
-
-def PrintWeatherList(tags):
-    global WeatherDoc
-    if not checkDocument():
-        return None
-
-    # doc = WeatherDoc.childNodes
-    # res = doc[0].childNodes
-    # for body in res:
-    #     if body.nodeName == "body": # 엘리먼트 중 body를 찾는다.
-    #         items = body.firstChild
-    #         # item = items.childNode
-    # for item in items:
-    #             if item.nodeName == "item":
-    #                 print("found item!")
-    #             print(item.nodeValue)
-    doc = WeatherDoc.childNodes
-    res = doc[0].childNodes
-    print("test")
-    body = res[1].childNodes
-    items = body[0].childNodes
+def getSpaceFcstData(tree):             # 동네예보정보에서 하늘상태, 강수상태, 온도, 최저온도, 최고온도 뽑아내는 함수
+    items = tree.find('body/items')
+    fcstData = []
     for item in items:
-        subitem = item.childNodes
-        for atom in subitem:
-            if atom.nodeName in tags:
-                print(atom.firstChild.nodeValue, '\n')
+        if item.find('fcstDate').text == time.strftime('%Y%m%d', time.localtime()): # fcstDate가 현재 날짜와 같은가?
+            if item.find('fcstTime').text == getCloseTime():  # 현재 시간 전의 fcstTime인지 확인한다.
+                if item.find('category').text == 'SKY' or item.find('category').text == 'PTY' or item.find('category').text == 'T3H': # category가 SKY, PTY, T3H, TMX, TMN 이면
+                    category = item.find('category').text
+                    fcstValue = item.find('fcstValue').text
+                    fcstData.append({category: fcstValue})       # category와 fcstValue를 dict 형태로 list.append 한다.
 
-WeatherDoc = loadXMLfromAPI()
-PrintWeatherList(['category', 'fcstValue'])
-##### run #####
-# while(loopFlag > 0):
-#     printMenu()
-#     menuKey = str(input ('select menu :'))
-#     launcherFunction(menuKey)
-# else:
-#     print("Thank you! Good Bye")
+            if item.find('category').text == 'TMN' or item.find('category').text == 'TMX':
+                category = item.find('category').text
+                fcstValue = item.find('fcstValue').text
+                fcstData.append({category: fcstValue})           # category와 fcstValue를 dict 형태로 list.append 한다.
+
+    return fcstData
+
+
+def getUltraViolet(UVDoc):               # 자외선지수 뽑아내는 함수
+    pass
+
+
+tree, now = Parser().TimeFcstDocument
+print(getSpaceFcstData(tree))
+
--- 개발/2014182019 박태준/parseXMLs.py
+++ 개발/2014182019 박태준/parseXMLs.py
@@ -1,18 +1,60 @@
 import urllib.request
 import urllib.parse
 from xml.dom.minidom import parseString
-
+from xml.etree import ElementTree
+import time
 # 동네예보 xml은 base_date를 넘겨줘야함.
+# 2019.05.17 : 필요한 Open API 파싱 완료.
+#              하지만 옵션들 고정적인게 아니라 현재 또는 원하는 date, time 지정해줄 수 있어야 하기 때문에 수정이 필요함.
+# 2019.05.22 : 동네예보 파싱시 date, time 해당시간에 맞게(baseTime에 가장 가까운 예보시간부터) 파싱해옴.
+
+# base_date = time.strftime('%Y%m%d', time.localtime())
+base_time = time.strftime('%H', time.localtime()) + '00'
 
 
+def get_baseDateAndTime():
+    base_date = time.strftime('%Y%m%d', time.localtime())
+    base_time = time.strftime('%H', time.localtime()) + '00'
+    if '0000' <= base_time < '0300':
+        base_date = str(int(base_date) - 1)
+        base_time = '2000'
+    elif '0300' <= base_time < '0600':
+        base_date = str(int(base_date) - 1)
+        base_time = '2300'
+    elif '0600' <= base_time < '0900':
+        base_time = '0200'
+    elif '0900' <= base_time < '1200':
+        base_time = '0500'
+    elif '1200' <= base_time < '1500':
+        base_time = '0800'
+    elif '1500' <= base_time < '1800':
+        base_time = '1100'
+    elif '1800' <= base_time < '2100':
+        base_time = '1400'
+    elif '2100' <= base_time < '0000':
+        base_time = '1700'
 
-def getForecastSpaceData():     # 동네예보조회 xml 파싱
+    return base_date, base_time
+
+
+def parseFcstPerTime():  # 동네예보조회 xml 파싱
     url = 'http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastSpaceData'
-    queryParams = '?' + 'ServiceKey=8KngOJTE%2Fh%2BjNJwkeXlJsC5d1ShWfQ9YadkSpoLeubDe9cekkO44ShcRAra7hjTk%2BYAzJEui5eYPFVGegxUngw%3D%3D' +\
-        '&base_date=20190517&base_time=0200&nx=60&ny=127&numOfRows=300' \
-        '&pageNo=1'
+    serviceKey = 'ServiceKey=8KngOJTE%2Fh%2BjNJwkeXlJsC5d1ShWfQ9YadkSpoLeubDe9cekkO44ShcRAra7hjTk%2BYAzJEui5eYPFVGegxUngw%3D%3D'
 
-    request = urllib.request.Request(url + queryParams)
+    baseDate ,baseTime = get_baseDateAndTime()
+    baseDate = 'base_date=' + baseDate
+    baseTime = 'base_time=' + baseTime
+    nx = 'nx=60'
+    ny = 'ny=127'
+
+    params = '?' + serviceKey + '&' + baseDate + '&' + baseTime + '&' + nx + '&' + ny + '&numOfRows=200&pageNo=1'
+
+
+    queryParams = '?' + 'ServiceKey=8KngOJTE%2Fh%2BjNJwkeXlJsC5d1ShWfQ9YadkSpoLeubDe9cekkO44ShcRAra7hjTk%2BYAzJEui5eYPFVGegxUngw%3D%3D' + \
+                  '&base_date=20190521&base_time=2000&nx=60&ny=127&numOfRows=200' \
+                  '&pageNo=1'
+
+    request = urllib.request.Request(url + params)
 
     try:
         resp = urllib.request.urlopen(request)
@@ -26,8 +68,10 @@ def getForecastSpaceData():     # 동네예보조회 xml 파싱
         xml = resp.read()
         print("XML Document loading complete.")
         dom = parseString(xml)
-        return dom
+        # print(dom.toprettyxml())
+        tree = ElementTree.fromstring( str(dom.toxml()))
 
+        return tree, get_baseDateAndTime()
 
 def getMiddleLandWeather():     # 중기육상예보 xml (3일~10일 후 예보 정보 구름많음 등..)
     url = 'http://newsky2.kma.go.kr/service/MiddleFrcstInfoService/getMiddleLandWeather'
@@ -48,7 +92,6 @@ def getMiddleLandWeather():     # 중기육상예보 xml (3일~10일 후 예보 
         xml = resp.read()
         print("XML Document loading complete.")
         dom = parseString(xml)
-        print(dom.toprettyxml())
         return dom
 
 
@@ -71,7 +114,6 @@ def getMiddleTemperature():         # 중기기온조회 xml
         xml = resp.read()
         print("XML Document loading complete.")
         dom = parseString(xml)
-        print(dom.toprettyxml())
         return dom
 
 
@@ -94,7 +136,6 @@ def getUltrvLifeList():        # 체감온도 xml
         xml = resp.read()
         print("XML Document loading complete.")
         dom = parseString(xml)
-        print(dom.toprettyxml())
         return dom
 
 
@@ -117,11 +158,30 @@ def CityAirPollution():                 # 시,도별 대기오염지수 xml
         xml = resp.read()
         print("XML Document loading complete.")
         dom = parseString(xml)
-        print(dom.toxml())
         return dom
 
-# TownDocument = getForecastSpaceData()             # 동네예보 xml DOM 객체에 저장
-# DaysWeatherDoc = getMiddleLandWeather()           # 중기예보 xml DOM 객체에 저장
-# DaysTemperatureDoc = getMiddleTemperature()       # 중기기온 xml DOM 객체에 저장
-# UltRVDoc = getUltrvLifeList()                     # 자외선지수 xml DOM 객체에 저장
-APDoc = CityAirPollution()
+
+# def parseXMLs():
+#     global TimefcstDocument, DaysWeatherDoc, DaysTemperatureDoc, UltRVDoc, APDoc
+#
+#     TimefcstDocument = getForecastSpaceData()             # 동네예보 xml DOM 객체에 저장
+#     DaysWeatherDoc = getMiddleLandWeather()           # 중기예보 xml DOM 객체에 저장
+#     DaysTemperatureDoc = getMiddleTemperature()       # 중기기온 xml DOM 객체에 저장
+#     UltRVDoc = getUltrvLifeList()                     # 자외선지수 xml DOM 객체에 저장
+#     APDoc = CityAirPollution()                        # 시,도별 PM10평균 xml DOM 객체에 저장
+
+parseFcstPerTime()
+
+class Parser():
+    def __init__(self):
+        self.TimeFcstDocument = parseFcstPerTime()
+        # self.DaysWeatherDoc = getMiddleLandWeather()
+        # self.DaysTemperatureDoc = getMiddleTemperature()
+        self.UVDoc = getUltrvLifeList()
+        # self.APDoc = CityAirPollution()
+
+    def print(self):
+        print(self.TimeFcstDocument.toprettyxml())
+
+
+
